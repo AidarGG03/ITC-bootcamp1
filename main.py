689baffb871f19d8573721f36ca7fc9e32054902
Aidar Asankadyrov <dspace438@gmail.com>
2023-09-20 13:08:25 +0600

    I added first days tasks

---
+++ main.py
@@ -0,0 +1,51 @@
+print((17*3)<(12*5))   #false
+print((12**3)>(13*7)) #false
+print((4**5)<(512+512)) #True
+print((17925)>(34**2)) #True
+print((17925)==(26*3))  #True
+print((1925)<(17*33))  #false
+print((1925)>(4394*4))  #False
+
+print((((5+7)*3)**2)/193432)
+
+A=5
+B=3
+C=11
+
+print(A>B)
+print(A<C)
+print((7%3)*4.8)
+
+print((5+5)==(5+5))
+print((3*2)!=(6*1))
+
+print(-21//10)
+
+A=2003
+B=2021
+print((B-A)+2)
+print((B-A)-2)
+
+print((25+75+10+95)/4)
+
+a=10.
+b=10.
+c=10.
+print((a+b)*c)
+
+a=6
+b=15
+c=20
+d=7
+e=4
+
+a=6
+e=4
+c=-2
+d=8
+b=30
+print((a-e**(b/d))%c)
+
+a=20
+b=100
+print((a/b)*100)
